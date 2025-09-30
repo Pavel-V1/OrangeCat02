6f6cd906536fe5d182522106809723e1b10cfed6
Pavel-V1 <pavelvoronov009@gmail.com>
2025-09-30 08:36:37 +0300

    +

---
+++ OOP/QuadTree/src/ru/vsu/cs/oop25/g11/voronov_p_a/quad_tree/QuadTree.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.oop25.g11.voronov_p_a.quad_tree;
+
+public class QuadTree {
+    public void print() {
+        System.out.println();
+        System.out.println("HeLLo");
+    }
+
+    public void greeting(String s) {
+        System.out.println();
+        System.out.println("Hello " + s + "!");
+    }
+}
